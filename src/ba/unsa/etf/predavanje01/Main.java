4f7429ce180cf5f698dd6db13eb9834c105237ad
beganov1c <tbeganovic1@etf.unsa.ba>
2021-10-22 14:15:50 +0200

    Initial commit

---
+++ src/ba/unsa/etf/predavanje01/Main.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.predavanje01;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara(int broj) {
+        int suma=0;
+
+        while(broj>0) {
+            suma+=broj%10;
+            broj/=10;
+        }
+
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Unesite broj n: ");
+        Scanner in = new Scanner(System.in);
+        int n;
+        n = in.nextInt();
+
+        System.out.println("Trazeni brojevi su: ");
+        for(int i=1; i<=n; i++) {
+            if(i%sumaCifara(i)==0) System.out.println(i);
+        }
+    }
+}
